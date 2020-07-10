6d546bb213dd61c8a46d3013bd56176a1d6bfc9f
Joshua-Robbins <40744098+Joshua-Robbins@users.noreply.github.com>
2020-07-09 18:11:08 -0600

    Flair (#1)
    Big ole detailed commit log
    
    * initial addition for 'flair'
    
    * fixing compile error
    
    Co-authored-by: Joshua Robbins <jrobbin@sandia.gov>

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
 int main(){
-  std::cout << "this thing on?" << std::endl;
+  std::cout << "This is SandBox version 1.0" << std::endl;
+
+  std::cout << "This code doesn't actually do anything" << std::endl;
 }
