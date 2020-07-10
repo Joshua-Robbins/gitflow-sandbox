5c7daa32d24f9fb146433c785b1fa4f99998f6ac
Joshua Robbins <jrobbin@sandia.gov>
2020-07-09 18:37:44 -0600

    added something else

--- src/main.cpp
+++ src/main.cpp
@@ -4,4 +4,6 @@ int main(){
   std::cout << "This is SandBox version 1.0" << std::endl;
 
   std::cout << "This code doesn't actually do anything" << std::endl;
+
+  std::cout << "Still doesn't" << std::endl;
 }
